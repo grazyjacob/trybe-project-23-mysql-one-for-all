d08b0e00d0826c3e8a8df0836b71de63729a3f07
Graziely Jacob <jacobgrazy@gmail.com>
2022-10-26 16:09:30 -0300

    Teste Req1

--- desafio1.sql
+++ desafio1.sql
@@ -1,26 +1,32 @@
 DROP DATABASE IF EXISTS SpotifyClone;
-
   CREATE DATABASE IF NOT EXISTS SpotifyClone;
-
-  CREATE TABLE SpotifyClone.tabela1(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
-
-  INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+  USE SpotifyClone;
+  CREATE TABLE client_table (
+      client_id INT PRIMARY KEY AUTO_INCREMENT,
+      client_name VARCHAR(50),
+      client_age INT 
+  ) ENGINE = InnoDB;
+  CREATE TABLE album_table (
+      album_id INT,
+      album_name VARCHAR(50),
+      artist_id INT,
+      music_id INT 
+  ) ENGINE = InnoDB;
+  CREATE TABLE historic_table (
+      historic_id INT,
+      client_id INT PRIMARY KEY AUTO_INCREMENT,
+      music_historic VARCHAR(100)
+   ) ENGINE = InnoDB;
+  CREATE TABLE plane_table (
+      plane_name VARCHAR(100),
+      plane_id INT PRIMARY KEY AUTO_INCREMENT
+  ) ENGINE = InnoDB;
+  CREATE TABLE follow_artists_table (
+      follow_artists VARCHAR(100),
+  ) ENGINE = InnoDB;
+  CREATE TABLE artists_table (
+      artist_name VARCHAR(100)
+  ) ENGINE = InnoDB;
+  CREATE TABLE musics_table (
+      music_name VARCHAR(100)
+  ) ENGINE = InnoDB;
